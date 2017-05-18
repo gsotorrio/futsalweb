0856aefed01bfe1c18677cb90fbe13e3c1dc616a
osotorrio <osotorrio@hotmail.com>
2017-05-18 11:54:14 +0100

    Set up web project

---
+++ src/Futsalweb.Client/Controllers/TeamsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Futsalweb.Client.Controllers
+{
+    public class TeamsController : Controller
+    {
+        // GET: teams/list
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
