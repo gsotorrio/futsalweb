dc264a0d9f06028175327e1975e7ead2d5687a65
gsotorrio <willybosko@gmail.com>
2017-05-25 09:59:59 +0200

    Preparing html for ko

--- src/Futsalweb.Client/Scripts/App/teamsList.js
+++ src/Futsalweb.Client/Scripts/App/teamsList.js
@@ -1,49 +1,5 @@
 "use strict";
 
-const addNewTeam = (team) => {
-    $("#tableTeams tr:last").after("<tr> <td>"
-        + team.teamId + 
-        "</td><td>" 
-        + team.teamName + 
-        "</td><td>" 
-        + team.teamCategory + 
-        "</td><td>"
-        + "<a href='#' class='edit'>Edit</a>" +
-        "</td><td>"
-        + "<a href='#' c;lass='update'>Update</a>" +
-        "</td><td>"
-        + "<a href='#' class='delete'>Delete</a>" +
-        "</td></tr>")
-}
+(function () {
 
-const linkEventClickDelete = () => {
-    $(".delete").click(function (event) {
-        let trTeam = event.target.parentElement.parentElement;
-
-        $.ajax({
-            url: "http://localhost:5159/",
-            methiod: "DELETE"
-        }).done(function (){
-            $().remove();
-        });
-    });
-}
-// add something for ask you if you are sure.
-
-
-const linkEventClickUpdate = () => {
-    $(".update").click(function (event) {
-        let trPlayer = event.target.parentElement.parentElement;
-        
-        $.get("http://localhost:5159/", function (team) {
-
-            $("#teamName").val(team.teamName);
-            $("teamCategory").val(team.teamCategory);
-        });
-    });
-}
-
-
-
-
-
+}) ();
