59be08fe20ffb5c96260e507edb58b1b2bf9a8b1
gsotorrio <willybosko@gmail.com>
2017-05-25 10:27:45 +0200

    Preparing crud teams

--- src/Futsalweb.Client/Scripts/App/teamsList.js
+++ src/Futsalweb.Client/Scripts/App/teamsList.js
@@ -2,4 +2,35 @@
 
 (function () {
 
+    // Variables
+    let teams = ko.observableArray();
+
+    let team = {
+        teamId: ko.observable(),
+        name: ko.observable(),
+        category: ko.observable()
+    };
+
+    // Public Functions
+    const clean = function () {
+        team.name("");
+        team.category("");
+    };
+
+    // ViewModel
+    let viewModel = {
+        clean: clean
+    };
+
+    // On initialize
+    $(function () {
+        console.log("Ready!!");
+
+        $.get("", function (data) {
+
+            teams(data);
+            ko.applyBindings(viewModel);
+        });
+    });
+
 }) ();
