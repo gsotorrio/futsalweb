dd77c504b76e234afc3f8c806241b0b62ef699e6
gsotorrio <willybosko@gmail.com>
2017-06-07 15:18:46 +0200

    Prepare the first view

--- src/Futsalweb.Client/Scripts/App/teamsList.js
+++ src/Futsalweb.Client/Scripts/App/teamsList.js
@@ -12,16 +12,6 @@
     };
 
     // Public Functions
-    const clean = function () {
-        team.id("");
-        team.name("");
-        team.category("");
-    };
-
-    const edit = function () {
-
-    };
-    
     const remove = function (team, event) {
         $.ajax({
             url: "http://localhost:5159/api/teams/" + team.id,
@@ -31,71 +21,21 @@
         });
     }; // messege: Are you sure???
 
-    const select = function (slectedTeam) {
-        $.get("http://localhost:5159/api/teams/" + slectedTeam.id, function (data) {
-            console.log(data);
+    const detail = function () {
 
-            team.id(data.id);
-            team.name(data.name);
-            team.category(data.category);
-        });
     };
 
-    const createUpdateTeam = function () {
-        let id = team.id();
+    const createNewTeam = function () {
 
-        if (!id){
-            let newTeam = {
-                name: team.name(),
-                category: team.category()
-            };
-
-            $.post("http://localhost:5159/api/teams", newTeam).done(function (data) {
-
-                teams.push(data);
-
-                clean();
-            });
-        }
-
-        else {
-            let updateDataTeam = {
-                id: team.id(),
-                name: team.name(),
-                category: team.category()
-            };
-
-            $.ajax({
-                type: "PUT",
-                url: "http://localhost:5159/api/teams",
-                contentType: "application/json",
-                data: JSON.stringify(updateDataTeam)
-            }).done(function () {
-
-                let indexTeam;
-
-                for (var i = 0; i < teams().length; i++) {
-                    if (teams()[i].id == updateDataTeam.id) {
-                        indexTeam = i;
-                    }
-                }
-
-                teams.replace(teams()[indexTeam], updateDataTeam);
-
-                clean();
-            });
-        }
     };
 
     // ViewModel
     let viewModel = {
         teams: teams,
         team: team,
-        edit: edit,
-        clean: clean,
         remove: remove,
-        select: select,
-        createUpdateTeam: createUpdateTeam
+        detail: detail,
+        createNewTeam: createNewTeam
     };
     
     // On initialize
