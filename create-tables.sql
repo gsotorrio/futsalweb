236effcd2b115487e970d594efc1acedae3616d3
osotorrio <osotorrio@hotmail.com>
2017-06-07 21:39:42 +0100

    Created Players and Coaches tables

--- create-tables.sql
+++ create-tables.sql
@@ -1,6 +1,55 @@
 ﻿USE [FutsalwebDB]
 GO
-/****** Object:  Table [dbo].[Teams]    Script Date: 30-May-17 8:03:58 PM ******/
+/****** Object:  Table [dbo].[Coaches]    Script Date: 07-Jun-17 9:38:35 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Coaches](
+	[Id] [uniqueidentifier] NOT NULL,
+	[TeamId] [uniqueidentifier] NOT NULL,
+	[Name] [varchar](100) NOT NULL,
+	[Surname] [varchar](100) NOT NULL,
+	[Birthdate] [date] NULL,
+	[Role] [varchar](100) NULL,
+ CONSTRAINT [PK_Coaches] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[Players]    Script Date: 07-Jun-17 9:38:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Players](
+	[Id] [uniqueidentifier] NOT NULL,
+	[TeamId] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[Surname] [nvarchar](100) NOT NULL,
+	[Birthdate] [date] NULL,
+	[Height] [float] NULL,
+	[Weight] [float] NULL,
+	[StrongLeg] [varchar](50) NULL,
+	[Status] [varchar](50) NULL,
+ CONSTRAINT [PK_Players] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[Teams]    Script Date: 07-Jun-17 9:38:36 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -15,4 +64,14 @@ CREATE TABLE [dbo].[Teams](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
+GO
+ALTER TABLE [dbo].[Coaches]  WITH CHECK ADD  CONSTRAINT [FK_Coaches_Teams] FOREIGN KEY([TeamId])
+REFERENCES [dbo].[Teams] ([Id])
+GO
+ALTER TABLE [dbo].[Coaches] CHECK CONSTRAINT [FK_Coaches_Teams]
+GO
+ALTER TABLE [dbo].[Players]  WITH CHECK ADD  CONSTRAINT [FK_Players_Teams] FOREIGN KEY([TeamId])
+REFERENCES [dbo].[Teams] ([Id])
+GO
+ALTER TABLE [dbo].[Players] CHECK CONSTRAINT [FK_Players_Teams]
 GO
