db21fdfb8aa963b9ea9d30cd133605e4d3bf2e1a
gsotorrio <willybosko@gmail.com>
2017-06-10 09:32:45 +0200

    Preparing managerWizard

--- src/Futsalweb.Client/Scripts/App/managerWizard.js
+++ src/Futsalweb.Client/Scripts/App/managerWizard.js
@@ -1,1 +1,23 @@
 ﻿"use strict";
+
+(function () {
+
+    // Variables
+    let team = {
+        id: ko.observable(),
+        name: ko.observable(),
+        category: ko.observable()
+    };
+
+    // Public Functions
+    const createTeam = function () {
+
+    };
+
+
+    // ViewModel
+    let viewModel = {
+        team: team,
+        createTeam: createTeam
+    };
+})();
