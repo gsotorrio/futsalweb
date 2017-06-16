81eb74303d90c29f446cd6ee74964f2009c7a9c0
gsotorrio <willybosko@gmail.com>
2017-06-16 15:42:59 +0200

    Preparing coach wizard

--- src/Futsalweb.Client/Scripts/App/coachesWizard.js
+++ src/Futsalweb.Client/Scripts/App/coachesWizard.js
@@ -20,33 +20,53 @@
         coach.role("");
     };
 
-    const createCoach = function () {
-        let newCoach = {
-            teamId: window.location.search.substr(8),
-            name: coach.name(),
-            surname: coach.surname(),
-            birthdate: coach.birthdate(),
-            role: coach.role()
-        };
+    const putDataCoachInForm = function (selectedCoach) {
+        $.get("http://localhost:5159/api/coaches/" + selectedCoach.id, function (data) {
+            console.log(data);
 
-        $.post("http://localhost:5159/api/coaches", newCoach).done(function (data) {
-
-            coaches.push(data);
-            cleanFormCoach();
-            //window.location.href = "http://localhost:5159";
+            coach.id(data.id);
+            coach.name(data.name);
+            coach.surname(data.surname);
+            coach.birthdate(data.birthdate);
+            coach.role(data.role);
         });
     };
 
     const removeCoach = function (coach, event) {
         $.ajax({
-            url: "http://localhost:5159/api/coaches/" + player.id,
+            url: "http://localhost:5159/api/coaches/" + coach.id,
             method: "DELETE"
         }).done(function () {
             coaches.remove(coaches);
         });
     };
 
+    const createCoach = function () {
+        let coachId = coach.id();
 
+        if (!coachId){
+            let newCoach = {
+                teamId: window.location.search.substr(8),
+                name: coach.name(),
+                surname: coach.surname(),
+                birthdate: coach.birthdate(),
+                role: coach.role()
+            };
+
+            $.post("http://localhost:5159/api/coaches", newCoach).done(function (data) {
+
+                coaches.push(data);
+                cleanFormCoach();
+            });
+        }
+    };
+ 
+
+    const goTeamList = function () {
+        //window.location.href = "http://localhost:5159";
+        console.log("ouh yeah!!!");
+    };
+ 
     // ViewModel
     let viewModel = {
         //List
@@ -70,8 +90,10 @@
         coaches: coaches,
         coach: coach,
         cleanFormCoach: cleanFormCoach,
+        putDataCoachInForm: putDataCoachInForm,
+        removeCoach: removeCoach,
         createCoach: createCoach,
-        removeCoach: removeCoach
+        goTeamList: goTeamList
     };
 
     // On initialize
