a6454755fbd7c5398a306df873b07eede144780f
gsotorrio <willybosko@gmail.com>
2017-08-04 10:46:34 +0200

    Preparing gamesDataWizard.js

--- src/Futsalweb.Client/Scripts/App/gamesDataWizard.js
+++ src/Futsalweb.Client/Scripts/App/gamesDataWizard.js
@@ -1,5 +1,46 @@
 ﻿"use strict";
 
 (function () {
+    // Variables
+    const httpAjax = new HttpAjax();
+
+    let gameData = {
+        idGame: ko.observable(),
+        homeTeam: ko.observable(),
+        visitTeam: ko.observable(),
+        dateGame: ko.observable(),
+        timeGame: ko.observable(),
+        placeGame: ko.observable(),
+        typeGame: ko.observable()
+    };
+
+    // Public Functions
+    const createUpdateGame = () => {
+        let newGame = {
+            idGame: gameData.idGame(),
+            homeTeam: gameData.homeTeam(),
+            visitTeam: gameData.visitTeam(),
+            dateGame: gameData.dateGame(),
+            timeGame: gameData.timeGame(),
+            placeGame: gameData.placeGame(),
+            typeGame: gameData.typeGame()
+        };
+
+        if (newGame.idGame){
+            console.log(newGame)
+        }
+        else {
+            navigateBetewnViews("/Games/PlayersGame");
+            console.log(newGame);
+        }
+    };
+
+    // View Model
+    const viewModel = {
+        gameData: gameData,
+        createUpdateGame: createUpdateGame
+    };
+    // On initialize
+    ko.applyBindings(viewModel);
 
 })();
