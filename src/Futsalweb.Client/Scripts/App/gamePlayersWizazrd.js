0d7b0dbee450fe3c225e84bd3d2b80c504a7aced
gsotorrio <willybosko@gmail.com>
2017-08-06 16:35:05 +0200

    Preparing gamePlayersWizard.js

--- src/Futsalweb.Client/Scripts/App/gamePlayersWizazrd.js
+++ src/Futsalweb.Client/Scripts/App/gamePlayersWizazrd.js
@@ -7,18 +7,40 @@
     let players = ko.observableArray();
 
     let player = {
-        idPlayer: ko.observable(),
-        playername: ko.observable(),
+        playerId: ko.observable(),
+        playerName: ko.observable(),
         playerSurname: ko.observable(),
         playerPosition: ko.observable(),
         playerNumber: ko.observable()
     };
     // Public Functions
+    const cleanForm = () => {
+        player.playerId("");
+        player.playerName("");
+        player.playerSurname("");
+        player.playerPosition("");
+        player.playerNumber("");
+    };
+
+    const createNewPlayer = () => {
+        let newPlayer = {
+            playerId: player.playerId(),
+            playerName: player.playerName(),
+            playerSurname: player.playerSurname(),
+            playerPosition: player.playerPosition(),
+            playerNumber: player.playerNumber()
+        };
+
+        players.push(newPlayer);
+        cleanForm();
+    };
 
     // ViewModel
     let viewModel = {
         players: players,
-        player: player
+        player: player,
+        cleanForm: cleanForm,
+        createNewPlayer: createNewPlayer
     };
     // On initialize
     ko.applyBindings(viewModel);
@@ -33,8 +55,8 @@
                 
             for (var i = 0; i < data.length; i++) {
                 let playerData = {
-                    idPlayer: data[i].id,
-                    playername: data[i].name,
+                    playerId: data[i].id,
+                    playerName: data[i].name,
                     playerSurname: data[i].surname,
                     playerPosition: data[i].position,
                     playerNumber: data[i].number
