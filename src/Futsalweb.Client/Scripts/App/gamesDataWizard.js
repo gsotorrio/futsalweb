efecacfb6dddbe5c25fff3bc66016f5d9978d24e
gsotorrio <willybosko@gmail.com>
2017-09-05 13:01:00 +0200

    Changing games/manager and gameDateWizard.js

--- src/Futsalweb.Client/Scripts/App/gamesDataWizard.js
+++ src/Futsalweb.Client/Scripts/App/gamesDataWizard.js
@@ -5,11 +5,8 @@
     const httpAjax = new HttpAjax();
     const router = new Router();
 
-    let teamsHome = ko.observableArray();
-    let selectTeamHome = ko.observable();
-
-    let teamsGuest = ko.observableArray();
-    let selectTeamGuest = ko.observable();
+    let myTeams = ko.observableArray();
+    let selectMyTeam = ko.observable();
 
     let hiddeHomeTextBox = ko.observableArray([]);
     let displayTeamHome = ko.observable(true);
@@ -18,31 +15,13 @@
     let displayTeamGuest = ko.observable(false);
 
     let gameData = {
+        rivalTeam: ko.observable(),
         idGame: ko.observable(),
         dateGame: ko.observable(),
         timeGame: ko.observable(),
-        placeGame: ko.observable(),
+        locationGame: ko.observable(),
         typeGame: ko.observable()
     };
-
-    // Private functions
-    selectTeamHome.subscribe(function (item) {
-        if (item == "Write yourself") {
-            displayTeamHome(false);
-            hiddeHomeTextBox.push("Some value");
-            displayTeamGuest(true);
-            hiddeGuestTextBox.splice(0, 1);
-        }
-    });
-
-    selectTeamGuest.subscribe(function (item) {
-        if (item == "Write rival team") {
-            displayTeamHome(true);
-            hiddeHomeTextBox.splice(0, 1);
-            displayTeamGuest(false);
-            hiddeGuestTextBox.push("some value");
-        }
-    });
 
     // Public Functions
     const createUpdateGame = () => {
@@ -66,10 +45,8 @@
 
     // View Model
     const viewModel = {
-        teamsHome: teamsHome,
-        selectTeamHome: selectTeamHome,
-        teamsGuest: teamsGuest,
-        selectTeamGuest: selectTeamGuest,
+        myTeams: myTeams,
+        selectMyTeam: selectMyTeam,
         hiddeHomeTextBox: hiddeHomeTextBox,
         displayTeamHome: displayTeamHome,
         hiddeGuestTextBox: hiddeGuestTextBox,
@@ -84,18 +61,13 @@
     $(function () {
         console.log("Ready!!!");
 
-        let arrayTeamsHome = [];
-        let arrayTeamsGuest = [];
+        let arraymyTeams = [];
         
         function showData(data) {
             for (var i = 0; i < data.length; i++){
-                arrayTeamsHome.push(data[i].name);
-                arrayTeamsGuest.push(data[i].name);
+                arraymyTeams.push(data[i].name);
             }
-            arrayTeamsHome.push("Write yourself");
-            arrayTeamsGuest.push("Write rival team")
-            teamsHome(arrayTeamsHome);
-            teamsGuest(arrayTeamsGuest);
+            myTeams(arraymyTeams);
         }
 
         const path = "/api/teams";
